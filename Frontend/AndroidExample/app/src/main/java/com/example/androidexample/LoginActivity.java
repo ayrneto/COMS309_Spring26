673c1596eda368d3bbc5ee6de032a1e2c6990b4a
Ayr Neto <ayrneto@macayr.student.iastate.edu>
2026-02-28 14:21:49 -0600

    Just comment

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -53,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void sendLoginRequest(String username, String password){
-        String url = "http://10.26.2.39:3000/api/login";  // Mockoon port
+        String url = "http://10.26.2.39:3000/api/login";  // Mockoon port for server
 
         // Create JSON object with username and password
         JSONObject loginData = new JSONObject();
