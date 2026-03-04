b24507afc3caf2379f32f8a14812dccc822524f8
Chakraborty <bcb43@iastate.edu>
2026-03-04 13:28:26 -0600

    Fix: Functionality

--- Backend/Roundtrip 1/springboot_example/src/main/java/onetoone/Counsellors/CounsellorProfileController.java
+++ Backend/Roundtrip 1/springboot_example/src/main/java/onetoone/Counsellors/CounsellorProfileController.java
@@ -12,6 +12,17 @@ import java.util.Optional;
 import onetoone.Users.User;
 import onetoone.Users.UserRepository;
 
+/**
+ * @author Boudhayan Chakraborty
+ *
+ * Services:
+ * 1. Create counsellor profile - add bio, profile pic, etc.
+ * 2. Update profile as a whole
+ * 3. Update bio
+ * 4. Update profile pic via link
+ * 5. Update the availability status
+ */
+
 @RestController
 @RequestMapping("/api/counsellors")
 public class CounsellorProfileController {
@@ -79,6 +90,9 @@ public class CounsellorProfileController {
         }
     }
 
+    /**
+     *  Creates the counsellor Profile
+     */
     @PutMapping("/{id}/profile")
     public ResponseEntity<?> upsert(@PathVariable Long id,
                                     @RequestBody CounsellorProfileRequest req) {
