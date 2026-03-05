cf6003cb8357334f7d3cc2f297c29a17d0f6701e
ayrneto <ayrneto@gmail.com>
2026-03-04 22:11:13 -0600

    Feat: changed the way the worry notes are displayed

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ApiConstants.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ApiConstants.java
@@ -3,15 +3,16 @@ package com.example.androidexample;
 public class ApiConstants {
 //    private ApiConstants() {}
 //    public static final String BASE_URL= "http://REDACTED:8080/";
-
 //    public static final String BASE_URL = "http://REDACTED:8080";
+//    public static final String BASE_URL = "http://10.27.139.8:3000";  // Mockoon port (Ayr's Mac)
+    public static final String BASE_URL = "http://REDACTED";  // Mockoon port (Ayr's PC)
 //    public static final String SIGNUP = BASE_URL + "/signup";
-//    public static final String LOGIN  = BASE_URL + "/login";
 //
 //    public static final String DELETE = BASE_URL + "/";
 //
 //     //Admin
 //    public static final String ADMIN_UPDATE_USER = BASE_URL + "/api/admin/update/"; // + id
-public static final String BASE_URL = "http://REDACTED";
+    //public static final String BASE_URL = "http://REDACTED";
     public static final String USERS = BASE_URL + "/users";
+    public static final String LOGIN  = BASE_URL + "/login";
 }
--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/WorryNotes.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/WorryNotes.java
@@ -3,7 +3,9 @@ package com.example.androidexample;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,7 +53,8 @@ public class WorryNotes extends AppCompatActivity {
 
 
     private void displayNotes(JSONArray notes){
-        LinearLayout container = findViewById(R.id.)
+        LinearLayout container = findViewById(R.id.worriesContainer);
+        container.removeAllViews(); // This resets all the worries in view
 
         try{
             for(int i = 0; i < notes.length(); i++){
@@ -62,11 +65,23 @@ public class WorryNotes extends AppCompatActivity {
                 //ToDo: String dueDate = note.getString("dueDate");
                 //ToDo: String label = note.getString("label");
 
-                Log.d("Notes", "Title: " + title);
+                addNoteCard(container, title, content);
             }
         }catch(JSONException e){
             e.printStackTrace();
         }
+    }
+
+    private void addNoteCard(LinearLayout container, String title, String content){
+        View cardView = getLayoutInflater().inflate(R.layout.note_card, container, false);
+
+        TextView titleView = cardView.findViewById(R.id.worryTitle);
+        TextView contentView = cardView.findViewById(R.id.worryDueDate);
+
+        titleView.setText(title);
+        contentView.setText(content);
+
+        container.addView(cardView);
     }
 
 
