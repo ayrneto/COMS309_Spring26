804e22360c0151223701f28d33eb66503ecf70f3
ayrneto <ayrneto@gmail.com>
2026-04-02 23:13:52 -0500

    Feat: task assignment for the Therapist usre. Basic setup

---
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/AssignTaskActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+
+public class AssignTaskActivity extends AppCompatActivity{
+
+}
