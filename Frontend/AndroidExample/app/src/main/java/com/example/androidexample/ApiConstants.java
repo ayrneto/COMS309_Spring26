1fc8e2533e093753aba746f83bb04e959ca9b0c8
Nakshatra Gupta <ng1308@iastate.edu>
2026-04-08 22:51:58 -0500

    Fixed Users endpoint

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ApiConstants.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/ApiConstants.java
@@ -9,7 +9,7 @@ public class ApiConstants {
 
     public static final String SIGNUP       = BASE_URL + "/users/signup";
     public static final String LOGIN        = BASE_URL + "/users/login";
-    public static final String USERS        = BASE_URL + "/users";
+    public static final String USERS        = BASE_URL + "/api/admin/users";
     public static final String DELETE       = BASE_URL + "/users/";
     public static final String EDIT         = BASE_URL + "/api/admin/update/";
     public static final String ADMIN_DELETE = BASE_URL + "/api/admin/";
@@ -61,7 +61,7 @@ public class ApiConstants {
 //    public static final String WS_BASE_URL  = "ws://REDACTED";
 //    public static final String SIGNUP       = BASE_URL + "/users/signup";
 //    public static final String LOGIN        = BASE_URL + "/users/login";
-//    public static final String USERS        = BASE_URL + "/users";
+//    public static final String USERS        = BASE_URL + "/api/admin/users";
 //    public static final String DELETE       = BASE_URL + "/users/";
 //    public static final String EDIT         = BASE_URL + "/api/admin/update/";
 //    public static final String ADMIN_DELETE = BASE_URL + "/api/admin/";
