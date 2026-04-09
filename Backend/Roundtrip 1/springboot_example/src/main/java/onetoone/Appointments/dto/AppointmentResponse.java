269a77b265eff6dd1d7a8af945e4a0d140d1f4c6
shreyy1111 <shreypatel617@gmail.com>
2026-04-08 20:24:42 -0500

    Minor changes in appointmentresponse dto

--- Backend/Roundtrip 1/springboot_example/src/main/java/onetoone/Appointments/dto/AppointmentResponse.java
+++ Backend/Roundtrip 1/springboot_example/src/main/java/onetoone/Appointments/dto/AppointmentResponse.java
@@ -8,4 +8,6 @@ public class AppointmentResponse {
     public String timeSlot;
     public String notes;
     public String status;
+    public String userName;       // name of the user who booked
+    public String counsellorName;
 }
